c834fee87d620bb4186f333ec625e0ac90d1bda0
Aditya Raj Singh <ranawataaditya06@gmail.com>
2025-05-31 10:48:37 +0530

    temporary commit

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,3 +1,55 @@
+// import {
+//   Sidebar,
+//   SidebarItem,
+//   SidebarItemGroup,
+//   SidebarItems,
+// } from "flowbite-react";
+// import { useEffect, useState } from "react";
+// import { HiUser, HiArrowSmRight } from "react-icons/hi";
+// import { useNavigate, useLocation } from "react-router-dom";
+
+// const DashSidebar = () => {
+//   const location = useLocation();
+//   const navigate = useNavigate();
+//   const [tab, setTab] = useState("");
+
+//   useEffect(() => {
+//     const urlParams = new URLSearchParams(location.search);
+//     const tabFromUrl = urlParams.get("tab");
+//     if (tabFromUrl) {
+//       setTab(tabFromUrl);
+//     }
+//   }, [location.search]);
+
+//   return (
+//     <Sidebar className="w-full md:w-56">
+//       <SidebarItems>
+//         <SidebarItemGroup>
+//           <SidebarItem
+//             active={tab === "profile"}
+//             icon={HiUser}
+//             label={"User"}
+//             labelColor="dark"
+//             onClick={() => navigate("/dashboard?tab=profile")}
+//           >
+//             Profile
+//           </SidebarItem>
+//           <SidebarItem
+//             icon={HiArrowSmRight}
+//             className="cursor-pointer"
+//             onClick={() => console.log("Sign out")}
+//           >
+//             Sign Out
+//           </SidebarItem>
+//         </SidebarItemGroup>
+//       </SidebarItems>
+//     </Sidebar>
+//   );
+// };
+
+// export default DashSidebar;
+
+
 import {
   Sidebar,
   SidebarItem,
@@ -6,13 +58,11 @@ import {
 } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { HiUser, HiArrowSmRight } from "react-icons/hi";
-import { useNavigate, useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const DashSidebar = () => {
   const location = useLocation();
-  const navigate = useNavigate();
   const [tab, setTab] = useState("");
-
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
@@ -20,25 +70,22 @@ const DashSidebar = () => {
       setTab(tabFromUrl);
     }
   }, [location.search]);
-
   return (
     <Sidebar className="w-full md:w-56">
       <SidebarItems>
         <SidebarItemGroup>
-          <SidebarItem
-            active={tab === "profile"}
-            icon={HiUser}
-            label={"User"}
-            labelColor="dark"
-            onClick={() => navigate("/dashboard?tab=profile")}
-          >
-            Profile
-          </SidebarItem>
-          <SidebarItem
-            icon={HiArrowSmRight}
-            className="cursor-pointer"
-            onClick={() => console.log("Sign out")}
-          >
+          <Link to="/dashboard?tab=profile">
+            <SidebarItem
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"User"}
+              labelColor="dark"
+              as='div'
+            >
+              Profile
+            </SidebarItem>
+          </Link>
+          <SidebarItem icon={HiArrowSmRight} className="cursor-pointer">
             Sign Out
           </SidebarItem>
         </SidebarItemGroup>
